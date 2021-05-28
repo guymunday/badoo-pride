10fe73f4c5b887c160043b3203e1665948837939
guymunday <guymunday@gmail.com>
2021-05-28 11:20:29 +0100

    GA

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,19 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="icon" type="image/ico" href={favicon} />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2KEKKY153W"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `[  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2KEKKY153W');]`,
+          }}
+        />
       </Head>
       <GlobalStyles />
       <CookiesBanner />
