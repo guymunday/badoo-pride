c09f26d5e4d77e908ad86bddee3758ab35ce4924
mt-code <matthew.croston@btinternet.com>
2021-06-03 15:56:00 +0100

    feat: Add different GA tags for different locales

--- components/Analytics.js
+++ components/Analytics.js
@@ -1,27 +1,117 @@
 import Head from "next/head";
 import favicon from "../assets/favicon.ico";
+import {useRouter} from "next/router";
 
 export default function Analytics() {
-  return (
-    <>
-      <Head>
-        <link rel="icon" type="image/ico" href={favicon} />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-2KEKKY153W"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
+    const router = useRouter();
+    const { locale } = router;
 
-              gtag('config', 'G-2KEKKY153W');
-            `,
-          }}
-        />
-      </Head>
-    </>
-  );
+    if (locale === "es") {
+        return (
+            <>
+                <Head>
+                    <link rel="icon" type="image/ico" href={favicon} />
+
+                    <script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=UA-198498245-1"
+                    ></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+            
+                            gtag('config', 'UA-198498245-1');
+                        `,
+                        }}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-57BJD4N');
+                        `,
+                        }}
+                    />
+                </Head>
+            </>
+        );
+    }
+
+    if (locale === "fr") {
+        return (
+            <>
+                <Head>
+                    <link rel="icon" type="image/ico" href={favicon} />
+
+                    <script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=UA-198431476-1"
+                    ></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+            
+                            gtag('config', 'UA-198431476-1');
+                        `,
+                        }}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-N6W62BD');
+                        `,
+                        }}
+                    />
+                </Head>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <Head>
+                <link rel="icon" type="image/ico" href={favicon} />
+
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=UA-198483278-1"
+                ></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+            
+                            gtag('config', 'UA-198483278-1');
+                        `,
+                    }}
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-KR5VRQR');
+                        `,
+                    }}
+                />
+            </Head>
+        </>
+    );
 }
