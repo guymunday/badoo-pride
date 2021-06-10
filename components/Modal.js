84e39ab27bfc75eb3f5d5a063b2c9a476d3d21ae
guymunday <guymunday@gmail.com>
2021-06-10 10:35:25 +0100

    modal styles

--- components/Modal.js
+++ components/Modal.js
@@ -13,6 +13,8 @@ const StyledModalOverlay = styled.div`
   cursor: pointer;
   .modal-inner {
     padding: 30px;
+    width: 100%;
+    min-height: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
